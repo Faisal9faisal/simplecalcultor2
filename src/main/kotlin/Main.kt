6e1a1f7dec92afbf9449d67b6d22f567d855beed
Faisal <faisal.Nahi.ruh.k@tuwaiq.edu.sa>
2021-10-12 22:37:06 +0300

    simple calcu

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println(" Enter first number :")
+    val num1 = readLine()!!.toInt()
+    println("Please Enter second number :")
+    val num2 = readLine()!!.toInt()
+    println("Please enter operation (+,-,*,/) : ")
+    val op = readLine()!!
+
+    if (op == "+") {
+        println((num1+num2))
+    }
+    else {
+        if (op == "-") {
+            println((num1-num2))
+        }
+        else if (op == "*") {
+            println((num1*num2))
+        }    else if (op == "/") {
+            println((num1/num2))
+        }
+    }
+}
